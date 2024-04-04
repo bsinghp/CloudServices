f416aa67c4f1f584a33e58ae1092618f2d6f5cea
bsinghp <bsp3999@gmail.com>
2024-04-04 10:47:24 +0530

    adding classes

---
+++ src/main/java/org/scaler/CloudServicesApplication.java
@@ -0,0 +1,13 @@
+package org.scaler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudServicesApplication.class, args);
+	}
+
+}
